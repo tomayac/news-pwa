b4770f9501b1a1b9266d71d4aef87ec1438df3d7
tomayac <steiner.thomas@gmail.com>
2018-11-26 16:51:51 +0100

    Styling and loading improvements, variable fonts

--- public/js/lazyload.mjs
+++ public/js/lazyload.mjs
@@ -2,18 +2,20 @@
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio > 0) {
-        const target = entry.target;
-        observer.unobserve(target);
-        const tagName = target.tagName;
-        if (tagName === 'IMG') {
-          target.src = target.dataset.src;
-        } else if (tagName === 'SOURCE') {
-          target.srcset = target.dataset.srcset;
+        const targets = entry.target.querySelectorAll('img, source');
+        observer.unobserve(entry.target);
+        for (const target of targets) {
+          const tagName = target.tagName;
+          if (tagName === 'IMG') {
+            target.src = target.dataset.src;
+          } else if (tagName === 'SOURCE') {
+            target.srcset = target.dataset.srcset;
+          }
         }
       }
     });
   });
-  const entries = document.querySelectorAll('img[lazyload], source[lazyload]');
+  const entries = document.querySelectorAll('[lazyload]');
   for (const entry of entries) {
     observer.observe(entry);
   }
--- public/js/polyfills.js
+++
@@ -1,3 +0,0 @@
-if (!('IntersectionObserver' in window)) {
-  document.write('<script src="https://unpkg.com/intersection-observer@0.5.0/intersection-observer.js"><\/script>');
-}
