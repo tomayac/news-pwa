82a96af057a6039f2bfceb2140ac1145bdbf533f
tomayac <steiner.thomas@gmail.com>
2018-11-27 14:28:45 +0100

    Add compression, CORS, and H/2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const compression = require('compression');
 const path = require('path');
+const spdy = require('spdy');
+const http = require('http');
 
 const indexRouter = require('./routes/index');
 
@@ -12,9 +16,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(helmet());
+app.use(cors());
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(PORT, () => console.log(`News PWA listening on port ${PORT}!`));
+const server = spdy.createServer(http.Server, {
+  protocols: ['h2'],
+  protocol: 'h2',
+  ssl: false,
+  plain: true
+}, app);
+
+console.log(server)
+
+const listener = server.listen(PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
