f90d341140af4b28d85bed88873e36fdfcae2349
tomayac <steiner.thomas@gmail.com>
2018-12-03 17:32:36 +0100

    Add section view

--- public/js/main.mjs
+++ public/js/main.mjs
@@ -1,10 +1,26 @@
 (() => {
-  document.querySelector('main').addEventListener('click', (event) => {
+  const main = document.querySelector('main');
+
+  main.addEventListener('click', (event) => {
     const target = event.target;
     const expandTargets = ['H1', 'H2', 'PICTURE', 'IMG', 'FIGCAPTION'];
     if (expandTargets.indexOf(target.nodeName) !== -1) {
       const details = target.closest('article').querySelector('details');
       details.open = !details.open;
     }
+  });
+
+  // Needed for calculating image dimensions
+  document.documentElement.style.setProperty('--main-current-width',
+      `${main.offsetWidth}px`);
+
+  // Debounced `onresize`
+  let resizeTimer;
+  window.addEventListener('resize', (e) => {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      document.documentElement.style.setProperty('--main-current-width',
+          `${main.offsetWidth}px`);
+    }, 250);
   });
 })();
